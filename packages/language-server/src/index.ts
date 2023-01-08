d5517634802084b2390fff2961a9fc0967529db9
johnsoncodehk <johnsoncodehk@gmail.com>
2023-01-08 22:44:54 +0800

    feat: add packages

--- packages/language-server/src/index.ts
+++ packages/language-server/src/index.ts
@@ -1,46 +1,13 @@
-import { languageModule, Html1File } from '@html1-language-tools/language-core';
-import createEmmetPlugin from '@volar-plugins/emmet';
-import createHtmlPlugin from '@volar-plugins/html';
-import createCssPlugin from '@volar-plugins/css';
-import { createConnection, startLanguageServer, LanguageServerPlugin, Diagnostic } from '@volar/language-server/node';
+import createPugPlugin from '@volar-plugins/pug';
+import createPugBeautifyPlugin from '@volar-plugins/pug-beautify';
+import { createConnection, startLanguageServer, LanguageServerPlugin } from '@volar/language-server/node';
 
 const plugin: LanguageServerPlugin = () => ({
-	extraFileExtensions: [{ extension: 'html1', isMixedContent: true, scriptKind: 7 }],
-	getLanguageModules() {
-		return [languageModule];
-	},
+	extraFileExtensions: [{ extension: 'pug', isMixedContent: true }],
 	getLanguageServicePlugins() {
 		return [
-			createHtmlPlugin(),
-			createCssPlugin(),
-			createEmmetPlugin(),
-			// custom html1 plugin
-			(context) => ({
-				validation: {
-					onSyntactic(document) {
-
-						const file = context.documents.getVirtualFileByUri(document.uri);
-						if (!(file instanceof Html1File)) return;
-
-						const styleNodes = file.htmlDocument.roots.filter(root => root.tag === 'style');
-						if (styleNodes.length <= 1) return;
-
-						const errors: Diagnostic[] = [];
-						for (let i = 1; i < styleNodes.length; i++) {
-							errors.push({
-								severity: 2,
-								range: {
-									start: file.document.positionAt(styleNodes[i].start),
-									end: file.document.positionAt(styleNodes[i].end),
-								},
-								source: 'html1',
-								message: 'Only one style tag is allowed.',
-							});
-						}
-						return errors;
-					},
-				},
-			}),
+			createPugPlugin(),
+			createPugBeautifyPlugin(),
 		];
 	},
 });
--- packages/vscode-pug/src/client.ts
+++ packages/vscode-pug/src/client.ts
@@ -1,12 +1,15 @@
-import { DiagnosticModel, LanguageServerInitializationOptions } from '@volar/language-server';
+import { LanguageServerInitializationOptions, DiagnosticModel } from '@volar/language-server';
 import * as vscode from 'vscode';
 import * as lsp from 'vscode-languageclient/node';
-import { registerAutoInsertion } from '@volar/vscode-language-client';
 
 let client: lsp.BaseLanguageClient;
 
 export async function activate(context: vscode.ExtensionContext) {
 
+	const initializationOptions: LanguageServerInitializationOptions = {
+		diagnosticModel: DiagnosticModel.Pull, // not matter because pug diagnostic is very fast
+		disableFileWatcher: true, // don't care about file changes
+	};
 	const serverModule = vscode.Uri.joinPath(context.extensionUri, 'server.js');
 	const runOptions = { execArgv: <string[]>[] };
 	const debugOptions = { execArgv: ['--nolazy', '--inspect=' + 6009] };
@@ -22,25 +25,17 @@ export async function activate(context: vscode.ExtensionContext) {
 			options: debugOptions
 		},
 	};
-	const initializationOptions: LanguageServerInitializationOptions = {
-		// no need tsdk because language server do not have typescript features
-		// typescript: { tsdk: require('path').join(vscode.env.appRoot, 'extensions/node_modules/typescript/lib') },
-		diagnosticModel: DiagnosticModel.Pull,
-	};
 	const clientOptions: lsp.LanguageClientOptions = {
-		documentSelector: [{ language: 'html1' }],
+		documentSelector: [{ language: 'jade' }],
 		initializationOptions,
 	};
 	client = new lsp.LanguageClient(
-		'html1-language-server',
-		'HTML1 Language Server',
+		'volar-pug-language-server',
+		'Pug (Volar)',
 		serverOptions,
 		clientOptions,
 	);
 	await client.start();
-
-	// support for auto close tag
-	registerAutoInsertion(context, [client], document => document.languageId === 'html1');
 }
 
 export function deactivate(): Thenable<any> | undefined {
