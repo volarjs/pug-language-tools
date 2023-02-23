3f6ef13dd8ab7c9c6ef0c447386a2d83612aba4a
johnsoncodehk <johnsoncodehk@gmail.com>
2023-02-24 02:38:17 +0800

    feat: upgrade volar

--- packages/language-server/src/index.ts
+++ packages/language-server/src/index.ts
@@ -1,14 +1,18 @@
-import createPugPlugin from '@volar-plugins/pug';
-import createPugBeautifyPlugin from '@volar-plugins/pug-beautify';
+import * as createPugPlugin from '@volar-plugins/pug';
+import * as createPugBeautifyPlugin from '@volar-plugins/pug-beautify';
 import { createConnection, startLanguageServer, LanguageServerPlugin } from '@volar/language-server/node';
 
 const plugin: LanguageServerPlugin = () => ({
-	extraFileExtensions: [{ extension: 'pug', isMixedContent: true }],
-	getLanguageServicePlugins() {
-		return [
-			createPugPlugin(),
-			createPugBeautifyPlugin(),
-		];
+	tsconfigExtraFileExtensions: [],
+	diagnosticDocumentSelector: [{ language: 'jade' }],
+	extensions: {
+		fileRenameOperationFilter: [],
+		fileWatcher: [],
+	},
+	resolveConfig(config) {
+		config.plugins ??= {};
+		config.plugins.pug ??= createPugPlugin();
+		config.plugins['pug-beautify'] ??= createPugBeautifyPlugin();
 	},
 });
 
